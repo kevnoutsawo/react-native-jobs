c80be4bdcc550c12aec91184fbab29d6fcf04f70
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2023-04-10 22:48:50 +0100

    🐛 💄 Fixed placeholder logo display bug

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -12,10 +12,12 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
     >
       <TouchableOpacity style={styles.logoContainer}>
         <Image
-          source={{ uri: checkImageURL(job.employer_logo)
-            ? job.employer_logo
-            : '../../../../assets/images/job.jpg' }}
-          resizeMode="contain"
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode='contain'
           style={styles.logoImage}
         />
       </TouchableOpacity>
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -4,7 +4,6 @@ import { View, Text, Image } from 'react-native'
 import styles from './company.style'
 import { icons } from '../../../constants'
 import { checkImageURL } from '../../../utils'
-import placeholderLogo from '../../../assets/images/job.jpg'
 
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
@@ -14,7 +13,7 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
           source={{
             uri: checkImageURL(companyLogo)
               ? companyLogo
-              : placeholderLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           style={styles.logoImage}
         />
