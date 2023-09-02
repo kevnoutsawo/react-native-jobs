70eee52d6b5c6ece1cdbdd92ce4c782e826c3ead
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2023-09-02 14:01:44 +0100

    Set up EAS for building

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons, images, SIZES } from '../constants';
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, ScreenHerderBtn, Welcome } from '../components';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     const router = useRouter();
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,48 +1,50 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { RAPID_API_KEY } from '@env';
 
 const useFetch = (endpoint, query) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const options = {
-        method: 'GET',
-        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-        headers: {
-            'X-RapidAPI-Key': RAPID_API_KEY,
-            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-        },
-        params: { ...query }
-    };
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': RAPID_API_KEY,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
+    params: { ...query },
+  };
 
-    const fetchData = async () => {
-        setIsLoading(true);
+  const fetchData = async () => {
+    setIsLoading(true);
 
-        try {
-            const response = await axios.request(options);
+    try {
+      const response = await axios.request(options);
 
-            setData(response.data.data);
-            setIsLoading(false);
-        } catch (error) {
-            setError(error);
-            alert('There is an error');
-        } finally {
-            setIsLoading(false);
-        }
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert(
+        `This error occured : ${error}. The server is currently unavailable.`
+      );
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const refetch = () => {
-        setIsLoading(true);
-        fetchData();
-    }
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
 
-    return {data, isLoading, error, refetch};
-}
+  return { data, isLoading, error, refetch };
+};
 
 export default useFetch;
